709a963e17f0b220afcbc039a8fd97349cd45037
Valentin Koch <valentin.koch@maibornwolff.de>
2021-11-04 16:09:24 +0100

    add time-sensitive timeline

--- app.py
+++ app.py
@@ -15,19 +15,22 @@ class BackendServer:
         self._app = Flask(__name__)
         self._app.add_url_rule('/up', 'up', self.up)
         self._app.add_url_rule('/get_tick', 'get_tick', self.return_tick)
-        self._app.add_url_rule('/get_timeline', 'get_timeline', self.get_timeline)
+        self._app.add_url_rule('/get_timeline', 'get_timeline', self._get_ticked_timeline)
         self._app.add_url_rule('/reset_tick', 'reset_tick', self.reset_counter)
 
 
         self.time_series = pd.read_csv("time_series.csv")
         self.time_series["unix"] = pd.to_datetime(self.time_series["time"]).astype(np.int64) // 10**6
         self.counter=0
-        self.max_counter=96
+        self.max_counter=96*5
 
         # self._app.register_error_handler(Exception, self.handle_error_generic)
         # self._app.register_error_handler(HTTPException, self.handle_error_http)
 
-        self.timeline = self._init_timeline()
+        self.timeline = []
+
+        self.plants = self.time_series.name.unique()
+        self.tick_timeline = {plant: None for plant in self.plants}
 
         self.time_series.drop(columns=["unix"], inplace=True)
         self.time_series["time"] = pd.to_datetime(self.time_series["time"])
@@ -61,11 +64,37 @@ class BackendServer:
     def up(self):
         return "OK"
 
+    def _get_ticked_timeline(self):
+        return jsonify(self.timeline + [x for x in self.tick_timeline.values() if x is not None])
+
+    def _process_tick_for_timeline(self, data):
+        # print(data)
+        for index, row in data.iterrows():
+            if row["command"] != 0:
+                if self.tick_timeline[row["name"]] is None:
+                    self.tick_timeline[row["name"]] = {
+                        "start": time.mktime(self.time_series.iloc[self.counter]["time"].timetuple()) * 10**3,
+                        "finish": time.mktime(self.time_series.iloc[self.counter]["time"].timetuple()) * 10**3,
+                        "name": row["name"],
+                        "value": row["command"]
+                    }
+                else:
+                    self.tick_timeline[row["name"]]["finish"] = time.mktime(self.time_series.iloc[self.counter]["time"].timetuple()) * 10**3
+            else:
+                if self.tick_timeline[row["name"]] is None:
+                    continue
+                else:
+                    self.timeline.append(self.tick_timeline[row["name"]])
+                    self.tick_timeline[row["name"]] = None
+        return
+
     def return_tick(self):
         if self.counter == self.max_counter:
-            return jsonify({"No more data"})
+            response = jsonify({"message": "No more data"})
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            return response
         data = self.time_series[self.time_series["time"] == self.time_series.iloc[self.counter]["time"]]
-        print(data.columns)
+        # print(data.columns)
         # data = self.time_series.iloc[self.counter].to_json()
         response = jsonify({
             "time": time.mktime(self.time_series.iloc[self.counter]["time"].timetuple()) * 10**3,
@@ -77,6 +106,7 @@ class BackendServer:
                 "pot_minus": data.pot_minus.sum()
             }]
         })
+        self._process_tick_for_timeline(data)
         self.counter += 1
         response.headers.add("Access-Control-Allow-Origin", "*")
         return response
